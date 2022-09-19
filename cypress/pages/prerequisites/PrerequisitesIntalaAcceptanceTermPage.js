e20d6f9179a17f6e4b8d912bdb177f4643f9ce54
Jessica Fuentes <jessica.g.fuentes@ext.leroymerlin.pt>
2022-09-19 12:31:20 +0100

    addition of the environment variable "TRANSLATION_INSTALA_CONFIRMATION_TEXT_ACCEPTANCE_CONDITIONS" for translation difference between UAT and production environment

--- cypress/pages/prerequisites/PrerequisitesIntalaAcceptanceTermPage.js
+++ cypress/pages/prerequisites/PrerequisitesIntalaAcceptanceTermPage.js
@@ -5,12 +5,17 @@ class PrerequisitesIntalaAcceptanceTermPage {
         sendAcceptanceTermsBtn: () => cy.get('.button-primary'),
         confirmationAcceptanceTermsLabel: () => cy.get('.container-fluid')
     };
+
+    /*
+           Function to confirm service conditions in INSTALA
+     */
+
     acceptTerms ()  {
         this.elements.conditionAcceptanceCheck().check();
         this.elements.signatureInput().click();
         this.elements.sendAcceptanceTermsBtn().click();
-        this.elements.confirmationAcceptanceTermsLabel().should('contain.text', 'Condições aceitas');
+        this.elements.confirmationAcceptanceTermsLabel().should('contain.text', Cypress.env('TRANSLATION_INSTALA_CONFIRMATION_TEXT_ACCEPTANCE_CONDITIONS'));
     };
-};
+}
 
 module.exports = new PrerequisitesIntalaAcceptanceTermPage();
