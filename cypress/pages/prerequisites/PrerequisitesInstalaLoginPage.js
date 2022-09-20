fd62172c5ec2577a8f5e9422b69b3a3614b91e6e
Jessica Fuentes <jessica.g.fuentes@ext.leroymerlin.pt>
2022-09-20 18:45:00 +0100

    fix login in instala

--- cypress/pages/prerequisites/PrerequisitesInstalaLoginPage.js
+++ cypress/pages/prerequisites/PrerequisitesInstalaLoginPage.js
@@ -2,17 +2,16 @@ class PrerequisitesInstalaLoginPage {
     elements = {
         usernameInstalaInput: () => cy.get('[name="pf.username"]'),
         passwordInstalaInput: () => cy.get('[name="pf.pass"]'),
-        signOnBtn: () => cy.get('[id="my_sign_on_button"]')
+        signOnBtn: () => cy.get('[id="my_sign_on_button"]'),
+        enterInstalaBtn: () => cy.get('[class="css-7fsdo1-button"]')
     };
 
     loginInstala () {
+        this.elements.enterInstalaBtn().click();
         this.elements.usernameInstalaInput().type(Cypress.env('ADEO_USERNAME'));
         this.elements.passwordInstalaInput().type(Cypress.env('ADEO_PASSWORD'));
-        cy.intercept('/newCockpit').as('homePage');
         this.elements.signOnBtn().click()
-        cy.wait('@homePage').then(() => {
-            cy.get('[id="breadcrumb-bar"]').should('contains.text', 'Cockpit');
-        })
+        cy.get('[id="breadcrumb-bar"]').should('contains.text', 'Cockpit');
     };
 }
 
