2f1884c8fb99cc4249e5e5722be2a2835ded0fd7
Jessica Fuentes <jessica.g.fuentes@ext.leroymerlin.pt>
2023-01-16 10:45:22 +0000

    code review

--- cypress/pages/prerequisites/PrerequisitesInstalaServiceOrderCreationPage.js
+++ cypress/pages/prerequisites/PrerequisitesInstalaServiceOrderCreationPage.js
@@ -37,46 +37,29 @@ class PrerequisitesInstalaServiceOrderCreationPage {
     };
 
     completeTheForm() {
-        if (Cypress.env('INSTALA_BASE_URL') === 'https://instala-uat.leroymerlin.pt'){
-            this.elements.storeCodeSelect().click();
-            this.elements.nameStoreInput().contains(Cypress.env('STORE_NAME')).click();;
-            this.generatePyxisNumber();
-            this.elements.taxNumberInput().clear().type(Cypress.env('CUSTOMER_FISCAL_ID'));
-            this.elements.customerNameInput().clear().type(Cypress.env('CUSTOMER_NAME'));
-            this.elements.emailInput().clear().type(Cypress.env('CUSTOMER_EMAIL'));
-            this.elements.phoneNumberCustomerInput().type(Cypress.env('CUSTOMER_PHONE_NUMBER'));
-            this.elements.zipInput().clear().type(Cypress.env('CUSTOMER_POSTAL_CODE'));
-            this.elements.countryInput().clear().type(Cypress.env('CUSTOMER_COUNTRY'));
-            this.elements.cityInput().clear().type(Cypress.env('CUSTOMER_CITY'));
-            this.elements.addressCustomerInput().clear().type(Cypress.env('CUSTOMER_ADDRESS'));
-            this.elements.numberAddressCustomerInput().clear().type(Cypress.env('CUSTOMER_ADDRESS_NUMBER'));
-            this.elements.serviceGroupSelect().click()
-            this.elements.nameServiceSelect().contains(Cypress.env('SERVICE_GROUP')).click();
-            this.typeDayDatePicker();
-            this.elements.turnOrderServiceCreateSelect().click().type('Manhã');
-            this.elements.turnSelect().click();
-            this.elements.addNewItemBtn().click();
-            this.elements.serviceCodeSelect().click().type('VISITA ORÇAMENTO');
-            this.elements.serviceSelect().click();
-            this.elements.serviceQuantityInput().type('1');
-            this.elements.serviceValueInput().type('25');
-            this.elements.saveServiceBtn().click();
-        }
-        if(Cypress.env('INSTALA_BASE_URL') === 'https://instala.leroymerlin.pt'){
-            this.elements.storeCodeSelect().click().type(Cypress.env('STORE_NAME'));
-            this.elements.nameStoreInput().click();
-            this.elements.pyxisNumberInput().type(Cypress.env('PYXIS_NUMBER'));
-            cy.intercept('GET', 'lm-instala-api/pyxis/*').as('searchPyxisOrder')
-            this.elements.searchPyxisOrderBtn().click();
-            cy.wait('@searchPyxisOrder').its('response.statusCode').should('eq', 200).then( () =>{
-                //this.elements.taxNumberInput().clear().type(Cypress.env('CUSTOMER_FISCAL_ID'));
-                //this.elements.emailInput().clear().type(Cypress.env('CUSTOMER_EMAIL'));
-                //this.elements.phoneNumberCustomerInput().clear().type(Cypress.env('CUSTOMER_PHONE_NUMBER'));
-                this.typeDayDatePicker();
-                this.elements.turnOrderServiceCreateSelect().click().type('Manhã');
-                this.elements.turnSelect().click();
-            });
-        }
+        this.elements.storeCodeSelect().click();
+        this.elements.nameStoreInput().contains(Cypress.env('STORE_NAME')).click();;
+        this.generatePyxisNumber();
+        this.elements.taxNumberInput().clear().type(Cypress.env('CUSTOMER_FISCAL_ID'));
+        this.elements.customerNameInput().clear().type(Cypress.env('CUSTOMER_NAME'));
+        this.elements.emailInput().clear().type(Cypress.env('CUSTOMER_EMAIL'));
+        this.elements.phoneNumberCustomerInput().type(Cypress.env('CUSTOMER_PHONE_NUMBER'));
+        this.elements.zipInput().clear().type(Cypress.env('CUSTOMER_POSTAL_CODE'));
+        this.elements.countryInput().clear().type(Cypress.env('CUSTOMER_COUNTRY'));
+        this.elements.cityInput().clear().type(Cypress.env('CUSTOMER_CITY'));
+        this.elements.addressCustomerInput().clear().type(Cypress.env('CUSTOMER_ADDRESS'));
+        this.elements.numberAddressCustomerInput().clear().type(Cypress.env('CUSTOMER_ADDRESS_NUMBER'));
+        this.elements.serviceGroupSelect().click()
+        this.elements.nameServiceSelect().contains(Cypress.env('SERVICE_GROUP')).click();
+        this.typeDayDatePicker();
+        this.elements.turnOrderServiceCreateSelect().click().type('Manhã');
+        this.elements.turnSelect().click();
+        this.elements.addNewItemBtn().click();
+        this.elements.serviceCodeSelect().click().type('Visita Orç');
+        this.elements.serviceSelect().click();
+        this.elements.serviceQuantityInput().type('1');
+        this.elements.serviceValueInput().type('25');
+        this.elements.saveServiceBtn().click();
     };
 
     typeDayDatePicker() {
