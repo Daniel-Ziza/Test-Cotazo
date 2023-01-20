8fbc5eda381c4e94df48d191cc8b655e940347b2
Jessica Fuentes <jessica.g.fuentes@ext.leroymerlin.pt>
2023-01-20 14:02:06 +0000

    incorporation of Portuguese characters to the randomString function

--- cypress/utils/index.js
+++ cypress/utils/index.js
@@ -1,7 +1,7 @@
 module.exports = {
     randomString: (length) => {
         let result           = '';
-        let characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        let characters       = 'AÁÃÂÀBCÇDEÉÊFGHIÍÎJKLMNOÓÔÕPQRSTUÚÛÜVWXYZaáàãbcçdeéêfghiíjklmnoóõpqrstuúvwxyz0123456789';
         let charactersLength = characters.length;
         for ( let i = 0; i < length; i++ ) {
             result += characters.charAt(Math.floor(Math.random() *
